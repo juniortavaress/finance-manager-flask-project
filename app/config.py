7ddd1c3ed5b6c0a62d4b59ffb2bbad6e9526402d
Jucelio Tavares Junior <123575526+juniortavaress@users.noreply.github.com>
2026-02-07 14:46:22 -0300

    Final Version 07.02

--- app/config.py
+++ app/config.py
@@ -1,24 +1,21 @@
-# import os
-# BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-
-# class Config:
-#     SECRET_KEY = os.environ.get("SECRET_KEY", "a3190c71717b80582c2b580d8bc02528")
-#     SQLALCHEMY_DATABASE_URI = "sqlite:///database.db"
-#     SQLALCHEMY_TRACK_MODIFICATIONS = False
-#     UPLOAD_FOLDER = "static/images"
-#     ENV = "development"
-#     DEBUG = True
-#     ENABLED_CRYPTOS_BRL = ["BTCBRL", "ETHBRL", "SOLBRL", "BNBBRL", "XRPBRL", "ADABRL"]
-
 import os
+
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+ROOT_DIR = os.path.dirname(BASE_DIR)
 
 class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY", "a3190c71717b80582c2b580d8bc02528")
+    
+    instance_path = os.path.join(ROOT_DIR, 'instance', 'database.db')
+    
+    if os.name == 'nt':  # If Windows (PC)
+        SQLALCHEMY_DATABASE_URI = "sqlite:///" + instance_path.replace("\\", "/")
+    else: # If Linux (PythonAnywhere)
+        SQLALCHEMY_DATABASE_URI = f"sqlite:////{instance_path}"
 
-    SQLALCHEMY_DATABASE_URI = f"sqlite:///{os.path.join(BASE_DIR, 'instance', 'database.db')}"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    UPLOAD_FOLDER = "static/images"
-    DEBUG = os.environ.get("FLASK_DEBUG", "False") == "True"
+    UPLOAD_FOLDER = os.path.join(BASE_DIR, "static", "images")
+    
+    # Debug enabled only on local PC
+    DEBUG = os.name == 'nt'
     ENABLED_CRYPTOS_BRL = ["BTCBRL", "ETHBRL", "SOLBRL", "BNBBRL", "XRPBRL", "ADABRL"]
-
--- main.py
+++ main.py
@@ -1,18 +1,7 @@
 
 from app import create_app
 
-
 app = create_app()
-
-
-
 
 if __name__ == "__main__":
     app.run(debug=app.config['DEBUG'])
-    # app.run(use_reloader=False, debug=app.config['DEBUG'])
-    
-
-# debug=false (mode de produção)
-#   -> não recarrega o servidor automaticamente
-#   -> erros genericos sem o traceback
-#   -> não expoe detalhes internos
